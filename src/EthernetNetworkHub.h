b9e8e0350f87f992b1d23606c40f2ebfb854cfb9
Mark Womack <mwomack@gmail.com>
2023-06-17 12:04:57 -0400

    Update EthernetNetworkHub.h
    Changed getTCPServer to getServer, and getUdpPort to getUDP.

--- src/EthernetNetworkHub.h
+++ src/EthernetNetworkHub.h
@@ -21,8 +21,8 @@ class EthernetNetworkHub : public NetworkHub {
     
     // NetworkHub methods
     void setHostIPAddress(IPAddress hostIPAddress);
-    NetworkUDP* getUdpPort(uint32_t portNum);
-    NetworkServer* getTCPServer(uint32_t portNum);
+    NetworkServer* getServer(uint32_t portNum);
+    NetworkUDP* getUDP(uint32_t portNum);
     void printStatus(Print* printer);
     
     // Returns the singleton instance of EthernetNetworkHub
